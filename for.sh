aaccceb0bfc5c44f4ab64dcf99b60e27ddb4464d
yangqinchuan <yangqinchuan@buding.cn>
2016-03-19 10:18:53 +0800

    m

---
+++ for.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Function:
+#   test for while
+# Hostory:
+#   2016年 3月19日 星期六 10时07分50秒 CST kis
+for ani in cat dog elephant 
+do 
+    echo "There are ${ani}s"
+done
---
+++ for_num.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+# Function:
+#   test for(()) do done
+# Hostory:
+#   2016年 3月19日 星期六 10时13分48秒 CST kis
+read -p "Please input your number:" number
+sum=0
+for (( i=1; i<=$number; i=i+1 ))
+do
+    sum=$(($sum+$i));
+done
+echo $sum
